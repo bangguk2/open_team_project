24085e528478c4560ed1d67729c1c101164c8e46
huiso jeon <sauroz@naver.com>
2022-11-26 23:05:24 +0900

    done!

--- OpenSourceProject1.0.0a/Assets/Scripts/Player/UnitMgr.cs
+++ OpenSourceProject1.0.0a/Assets/Scripts/Player/UnitMgr.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72f30d94994c790e505eac72b5f11bbaab3e3ddcb2985905905bf09b670b0a39
-size 4715
+oid sha256:b48e36e779ae4559ce63739a47f6f2ef58e2f6bbde600bc24c968029f82270a0
+size 5021
